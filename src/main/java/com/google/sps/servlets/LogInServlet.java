b61cbe05bd867ef2685a7284f6bd77511bdb9b3b
Meagan Konst <meagankonst@google.com>
2020-06-25 18:34:21 +0000

    Starts to implement a login service.

---
+++ src/main/java/com/google/sps/servlets/LogInServlet.java
@@ -0,0 +1,56 @@
+package com.google.sps.servlets;
+
+import com.google.appengine.api.datastore.DatastoreService;
+import com.google.appengine.api.datastore.DatastoreServiceFactory;
+import com.google.appengine.api.datastore.Entity;
+import com.google.appengine.api.datastore.FetchOptions;
+import com.google.appengine.api.datastore.PreparedQuery;
+import com.google.appengine.api.datastore.Transaction;
+import com.google.appengine.api.datastore.Key;
+import com.google.appengine.api.datastore.Query;
+import com.google.appengine.api.datastore.Query.SortDirection;
+//import com.google.appengine.api.oauth.OAuthService;
+//import com.google.appengine.api.oauth.OAuthServiceFactory;
+//import com.google.appengine.api.users.User;
+import com.google.appengine.api.users.UserService;
+import com.google.appengine.api.users.UserServiceFactory;
+import com.google.gson.Gson;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet("/login")
+public class LogInServlet extends HttpServlet {
+
+  final String ROOT_URL = "/";
+
+  @Override
+  public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
+    // TODO: Might have to use OAuthService and OAuthServiceFactory, but maybe not here.
+    hithr
+    response.setContentType("application/json;");
+    UserService userService = UserServiceFactory.getUserService();
+    boolean loggedIn = userService.isUserLoggedIn();
+    LoginInfo toSend;
+    if (loggedIn) {
+      toSend = new LoginInfo(loggedIn, userService.createLogoutURL(ROOT_URL));
+    } else {
+      toSend = new LoginInfo(loggedIn, userService.createLoginURL(ROOT_URL));
+    }
+    response.getWriter().println(new Gson().toJson(toSend));
+  }
+
+  private class LoginInfo {
+    private final boolean loggedIn;
+    private final String changeLogInStatusURL;
+
+    public LoginInfo(boolean loggedIn, String changeLogInStatusURL) {
+      this.loggedIn = loggedIn;
+      this.changeLogInStatusURL = changeLogInStatusURL;
+    }
+  }
+}
--- src/main/webapp/script.js
+++ src/main/webapp/script.js
@@ -12,3 +12,30 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
+function onIndexLoad() {
+  checkLogin();
+}
+
+function checkLogin() {
+  fetch('/login').then(response => response.json()).then(status => {
+    if(status.loggedIn){
+      restrictedTabs = document.getElementsByClassName('restricted-tab');
+      for(let element in restrictedTabs) {
+        element.className = 'nav-item nav-link';
+      }
+      document.getElementById('login-tab').innerText = 'Logout';
+      document.getElementById('login-tab').href = '"' + status.changeLogInStatusURL + '"';
+    } else {
+      document.getElementById('loggedOut').style.display = 'inline';
+      document.getElementById('login-message').innerHTML = 'To get started, please <a href="' + status.changeLogInStatusURL + '">login</a>.';
+      restrictedTabs = document.getElementsByClassName('restricted-tab');
+      for(let element in restrictedTabs) {
+        element.className = 'nav-item nav-link disabled';
+        //element.aria-disabled = "true";
+        //element.tab-index = -1;
+      }
+      document.getElementById('login-tab').innerText = 'Login';
+      document.getElementById('login-tab').href = '"' + status.changeLogInStatusURL + '"';
+    }
+  });
+}
