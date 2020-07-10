de31ad5c3121fa4fb449648eac286b6ef5db38ca
Claire Yang <34817835+apluscs@users.noreply.github.com>
2020-07-10 09:41:23 -0400

    Added Availability Dao lnterface (#66)
    * first draft
    
    * line length fixes
    
    * globally formatted
    
    * second draft
    
    * formatting
    
    * tweaks
    
    * added new funcs
    
    * formatting
    
    * tweaks
    
    * void return
    
    Co-authored-by: Claire <clairexy@google.com>

---
+++ src/main/java/com/google/sps/data/AvailabilityDao.java
@@ -0,0 +1,44 @@
+// Copyright 2019 Google LLC
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     https://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package com.google.sps.data;
+
+import java.util.List;
+import java.util.Optional;
+
+/**
+ * AvailabilityDao includes the basic methods anything managing Availability entities must support.
+ */
+public interface AvailabilityDao {
+  // Returns a list of all Availability's ranging from minTime to maxTime of a user.
+  // minTime and maxTime are in milliseconds.
+  public List<Availability> getInRangeForUser(String email, long minTime, long maxTime);
+
+  // Returns all Availability's across all users ranging from minTime to maxTime.
+  // minTime and maxTime are in milliseconds.
+  public List<Availability> getInRangeForAll(long minTime, long maxTime);
+
+  // Returns the Availability entity with specified id.
+  public Optional<Availability> get(long id);
+
+  // Adds an Availability object into storage.
+  public void create(Availability availability);
+
+  // Updates the specified id with the new availability.
+  public void update(Availability availability);
+
+  // Deletes all Availability entities for a user ranging from minTime to maxTime.
+  // minTime and maxTime are in milliseconds.
+  public void deleteInRangeForUser(String email, long minTime, long maxTime);
+}
