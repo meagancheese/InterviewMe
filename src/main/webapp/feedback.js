e7348e290c70c0fffc6ef68e60293a3dc103181d
Grant Commodore <gcommodore@google.com>
2020-08-06 12:33:23 -0400

    Updates the login/logout method name in feedback.js (#169)
    * fix
    
    * adds new logout methods

--- src/main/webapp/feedback.js
+++ src/main/webapp/feedback.js
@@ -14,7 +14,7 @@
 
 function onFeedbackLoad() {
   const loginInfo = getLoginInfo();
-  loginInfo.then(supplyLogoutLinkOrRedirectHome); 
+  loginInfo.then(ifLoggedOutRedirectHome); 
   loginInfo.then(getUserOrRedirectRegistration);
   loadFeedback(); 
 }
